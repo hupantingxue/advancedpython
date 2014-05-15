7a79c203fe77426d4c5b748440be823fbd1a8d05
hupantingxue <hupantingxue@126.com>
2014-05-15 19:37:38 +0800

    Decorator with args and print before begin and finish;

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python2.6
 #-*- coding:utf-8 -*-
 
+import functools
 import time
 
 def log(func):
     '''Decorator'''
+
+    @functools.wraps(func)
     def wrapper(*args, **kw):
-        print "call %s" % func.__name__
-        return func(*args, **kw)
+        print "begin call %s" % func.__name__
+        rv = func(*args, **kw)
+        print "end call %s" % func.__name__
+        return rv
     return wrapper
 
 @log
 def now():
     "The function which to be decorated"
     print time.strftime("%Y%m%d %H:%M:%S" ,time.localtime())
+    return 'just return1', 'return 2'
 
 if "__main__" == __name__:
     now()
--- decorator/decorator_withargs.py
+++ decorator/decorator_withargs.py
@@ -2,15 +2,20 @@
 #-*- coding:utf-8 -*-
 
 import time
+import functools
 
 def log(text):
     def decorator(func):
         '''Decorator'''
+
+        @functools.wraps(func)
         def wrapper(*args, **kw):
             '''wrapper'''
+
+            print "begin call %s with %s" % (func.__name__, text)
+            rv = func(*args, **kw)
             print "end call %s with %s" % (func.__name__, text)
-            return func(*args, **kw)
-        print "begin call %s with %s" % (func.__name__, text)
+            return rv
         return wrapper
     return decorator
 
