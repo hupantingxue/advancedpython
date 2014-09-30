b9af3e277c25faab6db0732f64baed423eb24b79
hupantingxue <hupantingxue@126.com>
2014-09-30 17:58:45 +0800

    pytest discovery function;

--- pytest/tests.py
+++ pytest/tests.py
@@ -1,3 +1,15 @@
+"""
+  pytest sample
+
+  The unittest module comes with a ‘discovery’ option.
+  Discovery is just built in to pytest.
+
+  pytest comes with 'discovery' function;
+    see also in: http://pytest.org/latest/example/pythoncollection.html
+
+  Run test:
+      python -m pytest tests.py -v
+"""
 from mydict import MyDict
 
 def test_mydict():
